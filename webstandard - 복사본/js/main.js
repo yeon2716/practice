bb243865fc5804501b1b4ab4c3cf8c4fbf16c54d
yeon2716 <145514579+yeon2716@users.noreply.github.com>
2023-11-06 00:40:17 +0900

    Add files via upload

---
+++ webstandard - 복사본/js/main.js
@@ -0,0 +1,29 @@
+﻿$('.tit .btn').click(function(){
+  $('.nav').slideToggle();
+  $(this).toggleClass('on');
+});
+
+/*
+.slideToggle() 
+보이지 않는 요소는 아래쪽으로 서서히 나타나게 하고, 보이는 요소는 위쪽으로 서서히 사라지게 합니다.
+
+$("button").click(function(){
+  $("p").slideToggle();
+});
+
+
+.toggleClass()
+$('.tit .btn').click(function(){
+  $(this).toggleClass('on');
+}); 
+
+*/
+
+
+
+$('.multiple-items').slick({
+  infinite: true,//무한반복
+  slidesToShow: 3,//한 화면에 보이는 갯수
+  slidesToScroll: 1,//버튼 클릭하면 넘어가는 갯수
+  dots: true,//이미지 아래 토글 유무
+});
